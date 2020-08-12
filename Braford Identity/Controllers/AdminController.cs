aede564e99bedf74fb1a53b093768d35c63ef46a
BrandonMefford <B.mefford1997@gmail.com>
2020-08-12 11:22:21 -0500

    creating admincontroller for an admin view, nothing made yet.

---
+++ Braford Identity/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Braford_Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Braford_Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProjectRole role)
+        {
+            var roleExist = await roleManager.RoleExistsAsync(role.RoleName);
+
+            return View();
+        }
+    }
+}
